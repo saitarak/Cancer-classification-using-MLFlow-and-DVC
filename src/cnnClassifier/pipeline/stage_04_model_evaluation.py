636191551fc62c302d6eeb448d222a8ba6f431f1
saitarak <saitarak.padarthi@gmail.com>
2024-04-25 22:33:46 +0200

    model final commit

--- src/cnnClassifier/pipeline/stage_04_model_evaluation.py
+++ src/cnnClassifier/pipeline/stage_04_model_evaluation.py
@@ -1,24 +1,23 @@
 from cnnClassifier.config.configuration import ConfigurationManager
-from cnnClassifier.components.model_trainer import Training
+from cnnClassifier.components.model_evaluation_mlflow import Evaluation
 from cnnClassifier import logger
 
-STAGE_NAME = "Training"
+STAGE_NAME = "Evaluation stage"
 
-class ModelTrainingPipeline:
+class EvaluationPipeline:
     def __init__(self):
         pass
     def main(self):
         config = ConfigurationManager()
-        training_config  = config.get_training_config()
-        training = Training(config = training_config)
-        training.get_base_model()
-        training.train_valid_generator()
-        training.train()
-        
+        evaluation_config  = config.get_evaluation_config()
+        evaluation = Evaluation(config = evaluation_config)
+        evaluation.evaluation()
+        #evaluation.log_into_mlflow()
+
 if __name__ == '__main__':
     try:
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = ModelTrainingPipeline()
+        obj = EvaluationPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
